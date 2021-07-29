4902665609dd34dcf99b18459de7ad6aeba8874d
michael rondon <mrondon72@gmail.com>
2021-07-30 01:36:10 +0200

    14. Header Circular

--- disenos_app/lib/src/widgets/headers.dart
+++ disenos_app/lib/src/widgets/headers.dart
@@ -1,5 +1,18 @@
 import 'package:flutter/material.dart';
 
+class HeaderBorderRedondeados extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.only( bottomLeft: Radius.circular( 70 ), bottomRight: Radius.circular( 70 ) ),
+        color: Color(0xff625AAB),
+      ),
+      height: 300,
+    );
+  }
+}
+
 class HeaderCuadrado extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
