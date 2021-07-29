4ee08531ee047fa3ab01f253b08b84d77654a0d8
michael rondon <mrondon72@gmail.com>
2021-07-30 01:33:07 +0200

    13. Header Cuadrado

---
+++ disenos_app/lib/src/pages/headers_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import 'package:disenos_app/src/widgets/headers.dart';
+
+class HeadersPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: HeaderCuadrado(),
+    );
+  }
+}
---
+++ disenos_app/lib/src/widgets/headers.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class HeaderCuadrado extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Color(0xff625AAB),
+      height: 300,
+    );
+  }
+}
