e644b7c1884d58e0fc3f0b82915173c3a8acf329
michael rondon <mrondon72@gmail.com>
2021-07-30 02:17:21 +0200

    23. Bonus: CustomPainter con Gradiente

--- disenos_app/lib/src/widgets/headers.dart
+++ disenos_app/lib/src/widgets/headers.dart
@@ -210,3 +210,61 @@ class _HeaderWavePainter extends CustomPainter {
     return true;
   }
 }
+
+class HeaderWaveGradient extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: CustomPaint(
+        painter: _HeaderWaveGradientPainter(),
+      ),
+      height: double.infinity,
+      width: double.infinity
+    );
+  }
+}
+
+class _HeaderWaveGradientPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final rect = new Rect.fromCircle(
+      center: Offset(0.0, 55.0), 
+      radius: 180
+    );
+
+    final Gradient gradiente = new LinearGradient(
+      begin: Alignment.topCenter,
+      colors: [
+        Color(0xff6D05E8),
+        Color(0xffC012FF),
+        Color(0xff6D05FA)
+      ],
+      end: Alignment.bottomCenter,
+      stops: [
+        0.2,
+        0.5,
+        1.0
+      ]
+    );
+
+    final lapiz = Paint()..shader = gradiente.createShader(rect);
+
+    lapiz.style = PaintingStyle.fill;
+    lapiz.strokeWidth = 20;
+
+    final path = new Path();
+    
+    path.lineTo(0, size.height * 0.25);
+    path.quadraticBezierTo(size.width * 0.25, size.height * 0.3, size.width * 0.5, size.height * 0.25);
+    path.quadraticBezierTo(size.width * 0.75, size.height * 0.2, size.width, size.height * 0.25);
+    path.lineTo(size.width, 0);
+    
+
+    canvas.drawPath(path, lapiz);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
