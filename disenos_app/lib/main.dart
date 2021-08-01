ab3fdcb57ba5a68e90f2821454467ee90e811feb
michael rondon <mrondon72@gmail.com>
2021-08-01 19:42:38 +0200

    60. Inicio del espacio para trabajar

--- disenos_app/lib/main.dart
+++ disenos_app/lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 
 // import 'package:disenos_app/src/pages/animaciones_page.dart';
-import 'package:disenos_app/src/pages/graficas_circulares_page.dart';
+// import 'package:disenos_app/src/pages/graficas_circulares_page.dart';
 // import 'package:disenos_app/src/pages/headers_page.dart';
+import 'package:disenos_app/src/pages/slideshow_page.dart';
 // import 'package:disenos_app/src/retos/cuadrado_animado_page.dart';
  
 void main() => runApp(MyApp());
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: GraficasCircularesPAge(),
+      home: SlideshowPage(),
       title: 'Diseños App',
     );
   }
