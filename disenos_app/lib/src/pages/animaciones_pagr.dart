5217f816379ecd5c84abc69cc8e180a9401b3a0f
michael rondon <mrondon72@gmail.com>
2021-08-01 15:34:21 +0200

    28. Animation y AnimationController

--- disenos_app/lib/src/pages/animaciones_pagr.dart
+++ disenos_app/lib/src/pages/animaciones_pagr.dart
@@ -16,7 +16,32 @@ class CuadradoAnimado extends StatefulWidget {
   _CuadradoAnimadoState createState() => _CuadradoAnimadoState();
 }
 
-class _CuadradoAnimadoState extends State<CuadradoAnimado> {
+class _CuadradoAnimadoState extends State<CuadradoAnimado> with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+  late Animation<double> rotacion;
+
+  @override
+  void initState() {
+    controller = new AnimationController(
+      duration: Duration( milliseconds: 4000 ),
+      vsync: this
+    );
+
+    rotacion = Tween(
+      begin: 0.0,
+      end: 2.0
+    ).animate( controller );
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return _Rectangulo();
