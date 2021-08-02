717aebe7260f458b9fdf21603d12debbc218ce90
michael rondon <mrondon72@gmail.com>
2021-08-02 22:31:59 +0200

    61. Mostrando SVGs en pantalla

--- disenos_app/lib/src/pages/slideshow_page.dart
+++ disenos_app/lib/src/pages/slideshow_page.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class SlideshowPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text('Hola mundo'),
+        child: SvgPicture.asset('assets/svgs/slide-1.svg')
       ),
     );
   }
