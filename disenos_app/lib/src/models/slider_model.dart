f2425673d49ebbdffb4e3db3e0e1b2505db5918c
michael rondon <mrondon72@gmail.com>
2021-08-02 22:54:03 +0200

    65. Provider - Compartir la pagina actual

---
+++ disenos_app/lib/src/models/slider_model.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SliderModel with ChangeNotifier {
+  double _currentPage = 0;
+
+  double get currentPage => this._currentPage;
+
+  set currentPage( double value ) {
+    this._currentPage = value;
+
+    notifyListeners();
+  }
+}
