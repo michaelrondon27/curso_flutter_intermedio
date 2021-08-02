8efd02685a9f78d6f23bd55c7634eff32f33d403
michael rondon <mrondon72@gmail.com>
2021-08-02 22:38:15 +0200

    62. PageView - Creando un Slideshow

--- disenos_app/lib/src/pages/slideshow_page.dart
+++ disenos_app/lib/src/pages/slideshow_page.dart
@@ -6,8 +6,41 @@ class SlideshowPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: SvgPicture.asset('assets/svgs/slide-1.svg')
+        child: _Slides()
       ),
+    );
+  }
+}
+
+class _Slides extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: PageView(
+        children: [
+          _Slide( 'assets/svgs/slide-1.svg' ),
+
+          _Slide( 'assets/svgs/slide-2.svg' ),
+
+          _Slide( 'assets/svgs/slide-3.svg' ),
+        ]
+      )
+    );
+  }
+}
+
+class _Slide extends StatelessWidget {
+  final String svg;
+
+  const _Slide( this.svg );
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: SvgPicture.asset( svg ),
+      height: double.infinity,
+      padding: EdgeInsets.all( 30 ),
+      width: double.infinity  
     );
   }
 }
