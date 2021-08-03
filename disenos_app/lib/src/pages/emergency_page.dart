4b6d924b7b4ef8c455382177bc2acf319fa872aa
michael rondon <mrondon72@gmail.com>
2021-08-03 21:16:39 +0200

    101. BotonGordo y nuestro IconHeader

--- disenos_app/lib/src/pages/emergency_page.dart
+++ disenos_app/lib/src/pages/emergency_page.dart
@@ -4,18 +4,58 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:disenos_app/src/widgets/headers.dart';
 import 'package:disenos_app/src/widgets/boton_gordo.dart';
 
+class ItemBoton {
+
+  final IconData icon;
+  final String texto;
+  final Color color1;
+  final Color color2;
+
+  ItemBoton( this.icon, this.texto, this.color1, this.color2 );
+}
+
 class EmergencyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final items = <ItemBoton>[
+      new ItemBoton( FontAwesomeIcons.carCrash, 'Motor Accident', Color(0xff6989F5), Color(0xff906EF5) ),
+      new ItemBoton( FontAwesomeIcons.plus, 'Medical Emergency', Color(0xff66A9F2), Color(0xff536CF6) ),
+      new ItemBoton( FontAwesomeIcons.theaterMasks, 'Theft / Harrasement', Color(0xffF2D572), Color(0xffE06AA3) ),
+      new ItemBoton( FontAwesomeIcons.biking, 'Awards', Color(0xff317183), Color(0xff46997D) ),
+      new ItemBoton( FontAwesomeIcons.carCrash, 'Motor Accident', Color(0xff6989F5), Color(0xff906EF5) ),
+      new ItemBoton( FontAwesomeIcons.plus, 'Medical Emergency', Color(0xff66A9F2), Color(0xff536CF6) ),
+      new ItemBoton( FontAwesomeIcons.theaterMasks, 'Theft / Harrasement', Color(0xffF2D572), Color(0xffE06AA3) ),
+      new ItemBoton( FontAwesomeIcons.biking, 'Awards', Color(0xff317183), Color(0xff46997D) ),
+      new ItemBoton( FontAwesomeIcons.carCrash, 'Motor Accident', Color(0xff6989F5), Color(0xff906EF5) ),
+      new ItemBoton( FontAwesomeIcons.plus, 'Medical Emergency', Color(0xff66A9F2), Color(0xff536CF6) ),
+      new ItemBoton( FontAwesomeIcons.theaterMasks, 'Theft / Harrasement', Color(0xffF2D572), Color(0xffE06AA3) ),
+      new ItemBoton( FontAwesomeIcons.biking, 'Awards', Color(0xff317183), Color(0xff46997D) ),
+    ];
+
+    List<Widget> itemMap = items.map( (item) => BotonGordo(
+      color1: item.color1,
+      color2: item.color2,
+      icon: item.icon,
+      onPress: () {},
+      texto: item.texto
+    )).toList();
+
     return Scaffold(
-      body: Center(
-        child: BotonGordo(
-          color1: Color(0xff6989F5),
-          color2: Color(0xff906EF5),
-          icon: FontAwesomeIcons.carCrash,
-          onPress: () {},
-          texto: 'Motor Accident'
-        ),
+      body: Stack(
+        children: [
+          Container(
+            child: ListView(
+              children: [
+                SizedBox( height: 80 ),
+
+                ...itemMap
+              ],
+            ),
+            margin: EdgeInsets.only( top: 200)
+          ),
+
+          PageHeader(),
+        ]
       )
     );
   }
