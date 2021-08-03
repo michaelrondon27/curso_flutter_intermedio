8c23329db459d5d109744e0bbf4c8f0d866725f5
michael rondon <mrondon72@gmail.com>
2021-08-03 20:53:27 +0200

    99. Informacion dentro del boton

--- disenos_app/lib/src/widgets/boton_gordo.dart
+++ disenos_app/lib/src/widgets/boton_gordo.dart
@@ -1,9 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class BotonGordo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return _BotonGordoBackground();
+    return Stack(
+      children: [
+        _BotonGordoBackground(),
+
+        Row(
+          children: [
+            SizedBox( height: 140, width: 40 ),
+
+            FaIcon(
+              FontAwesomeIcons.carCrash,
+              color: Colors.white,
+              size: 40
+            ),
+
+            SizedBox( width: 20 ),
+
+            Expanded(
+              child: Text(
+                'Motor Accident',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18
+                ),  
+              ),
+            ),
+
+            FaIcon(
+              FontAwesomeIcons.chevronRight,
+              color: Colors.white
+            ),
+
+            SizedBox( width: 40 )
+          ],
+          mainAxisAlignment: MainAxisAlignment.center,
+        )
+      ],
+    );
   }
 }
 
@@ -11,6 +48,22 @@ class _BotonGordoBackground extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular( 15 ),
+        child: Stack(
+          children: [
+            Positioned(
+              child: FaIcon(
+                FontAwesomeIcons.carCrash,
+                color: Colors.white.withOpacity( 0.2 ),
+                size: 150
+              ),
+              right: -20,
+              top: -20
+            )
+          ]
+        ),
+      ),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular( 15 ),
         boxShadow: [
