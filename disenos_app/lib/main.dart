da46758ab7f02464e5e70d4e5ee763003105c659
michael rondon <mrondon72@gmail.com>
2021-08-03 21:28:06 +0200

    108. Inicio de la aplicacion - SliverListApp

--- disenos_app/lib/main.dart
+++ disenos_app/lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 
 // import 'package:disenos_app/src/pages/animaciones_page.dart';
-import 'package:disenos_app/src/pages/emergency_page.dart';
+// import 'package:disenos_app/src/pages/emergency_page.dart';
 // import 'package:disenos_app/src/pages/graficas_circulares_page.dart';
 // import 'package:disenos_app/src/pages/headers_page.dart';
 // import 'package:disenos_app/src/pages/pinterest_page.dart';
 // import 'package:disenos_app/src/pages/slideshow_page.dart';
 // import 'package:disenos_app/src/retos/cuadrado_animado_page.dart';
+import 'package:disenos_app/src/pages/sliver_list_page.dart';
  
 void main() => runApp(MyApp());
  
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: EmergencyPage(),
+      home: SliverListPage(),
       title: 'Diseños App',
     );
   }
