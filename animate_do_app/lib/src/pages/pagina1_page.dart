bfaf17b1910e7fca91e2b253d01cc6e680f63eaf
michael rondon <mrondon72@gmail.com>
2021-08-04 17:03:56 +0200

    120. Animaciones basicas - Animate_do

--- animate_do_app/lib/src/pages/pagina1_page.dart
+++ animate_do_app/lib/src/pages/pagina1_page.dart
@@ -1,3 +1,5 @@
+import 'package:animate_do/animate_do.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -12,35 +14,57 @@ class Pagina1Page extends StatelessWidget {
             onPressed: () {},
           ),
 
-          IconButton(
-            icon: Icon( Icons.navigate_next ),
-            onPressed: () {},
+          SlideInLeft(
+            child: IconButton(
+              icon: Icon( Icons.navigate_next ),
+              onPressed: () {
+                Navigator.push(context, CupertinoPageRoute(builder: ( BuildContext context ) => Pagina1Page() ));
+              },
+            ),
+            from: 100,
           )
         ],
         centerTitle: true,
-        title: Text('Animate_do'),
+        title: FadeIn(
+          child: Text('Animate_do'),
+          duration: Duration( milliseconds: 500 ),
+        ),
       ),
       body: Center(
         child: Column(
           children: [
-            Icon( Icons.new_releases, color: Colors.blue, size: 40 ),
+            ElasticIn(
+              child: Icon( Icons.new_releases, color: Colors.blue, size: 40 ),
+              delay: Duration( milliseconds: 1100 ),
+            ),
 
-            Text('Titulo', style: TextStyle( fontSize: 40, fontWeight: FontWeight.w200 )),
+            FadeInDown(
+              child: Text('Titulo', style: TextStyle( fontSize: 40, fontWeight: FontWeight.w200 )),
+              delay: Duration( milliseconds: 200 ),
+            ),
 
-            Text('Soy un texto pequeño', style: TextStyle( fontSize: 20, fontWeight: FontWeight.w400 )),
+            FadeInDown(
+              child: Text('Soy un texto pequeño', style: TextStyle( fontSize: 20, fontWeight: FontWeight.w400 )),
+              delay: Duration( milliseconds: 800 ),
+            ),
 
-            Container(
-              color: Colors.blue,
-              height: 2,
-              width: 220
+            FadeInLeft(
+              child: Container(
+                color: Colors.blue,
+                height: 2,
+                width: 220
+              ),
+              delay: Duration( milliseconds: 1100 ),
             )
           ],
           mainAxisAlignment: MainAxisAlignment.center,
         )
       ),
-      floatingActionButton: FloatingActionButton(
-        child: FaIcon( FontAwesomeIcons.play ),
-        onPressed: () {},
+      floatingActionButton: ElasticInRight(
+        child: FloatingActionButton(
+          child: FaIcon( FontAwesomeIcons.play ),
+          onPressed: () {},
+        ),
       ),
     );
   }
