52f3f88b07775a2799ec2b7d60d0577c9565df56
michael rondon <mrondon72@gmail.com>
2021-08-04 16:29:48 +0200

    119. Diseño de la pagina 1

--- animate_do_app/lib/src/pages/pagina1_page.dart
+++ animate_do_app/lib/src/pages/pagina1_page.dart
@@ -1,11 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class Pagina1Page extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            icon: FaIcon( FontAwesomeIcons.twitter ),
+            onPressed: () {},
+          ),
+
+          IconButton(
+            icon: Icon( Icons.navigate_next ),
+            onPressed: () {},
+          )
+        ],
+        centerTitle: true,
+        title: Text('Animate_do'),
+      ),
       body: Center(
-        child: Text('Hola mundo')
+        child: Column(
+          children: [
+            Icon( Icons.new_releases, color: Colors.blue, size: 40 ),
+
+            Text('Titulo', style: TextStyle( fontSize: 40, fontWeight: FontWeight.w200 )),
+
+            Text('Soy un texto pequeño', style: TextStyle( fontSize: 20, fontWeight: FontWeight.w400 )),
+
+            Container(
+              color: Colors.blue,
+              height: 2,
+              width: 220
+            )
+          ],
+          mainAxisAlignment: MainAxisAlignment.center,
+        )
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: FaIcon( FontAwesomeIcons.play ),
+        onPressed: () {},
       ),
     );
   }
