2d0d9802c9c30486f156c6e41100ca71d94df370
michael rondon <mrondon72@gmail.com>
2021-08-04 17:43:10 +0200

    125. Animaciones con Provider

--- animate_do_app/lib/src/pages/navegacion_page.dart
+++ animate_do_app/lib/src/pages/navegacion_page.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:provider/provider.dart';
@@ -27,11 +28,19 @@ class BotonFlotante extends StatelessWidget {
       backgroundColor: Colors.pink,
       child: FaIcon( FontAwesomeIcons.play ),
       onPressed: () {
-        int numero = Provider.of<_NotificationModel>(context, listen: false).numero;
+        final notiModel = Provider.of<_NotificationModel>(context, listen: false);
+
+        int numero = notiModel.numero;
 
         numero++;
 
-        Provider.of<_NotificationModel>(context, listen: false).numero = numero;
+        notiModel.numero = numero;
+
+        if ( numero >= 2 ) {
+          final controller = notiModel.bounceController;
+
+          controller.forward( from: 0.0 );
+        }
       },
     );
   }
@@ -56,15 +65,23 @@ class BottomNavigation extends StatelessWidget {
               FaIcon( FontAwesomeIcons.bell ),
 
               Positioned(
-                child: Container(
-                  alignment: Alignment.center,
-                  child: Text('$numero', style: TextStyle( color: Colors.white, fontSize: 7)),
-                  decoration: BoxDecoration(
-                    color: Colors.pink,
-                    shape: BoxShape.circle
+                child: BounceInDown(
+                  animate: ( numero > 0 ) ? true : false,
+                  child: Bounce(
+                    child: Container(
+                      alignment: Alignment.center,
+                      child: Text('$numero', style: TextStyle( color: Colors.white, fontSize: 7)),
+                      decoration: BoxDecoration(
+                        color: Colors.pink,
+                        shape: BoxShape.circle
+                      ),
+                      height: 12,
+                      width: 12
+                    ),
+                    controller: (controller) => Provider.of<_NotificationModel>(context).bounceController = controller,
+                    from: 10,
                   ),
-                  height: 12,
-                  width: 12
+                  from: 10,
                 ),
                 right: 0,
                 top: 0
@@ -85,7 +102,16 @@ class BottomNavigation extends StatelessWidget {
 }
 
 class _NotificationModel with ChangeNotifier {
+  late AnimationController _bounceController;
   int _numero = 0;
+
+  AnimationController get bounceController => this._bounceController;
+  
+  set bounceController( AnimationController controller ) {
+    this._bounceController = controller;
+
+    notifyListeners();
+  }
 
   int get numero => this._numero;
 
