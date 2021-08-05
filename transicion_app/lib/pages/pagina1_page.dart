78e0f730ea1e8d6806018264708abb175681b1fc
michael rondon <mrondon72@gmail.com>
2021-08-05 22:52:29 +0200

    188. SlideTransition

--- transicion_app/lib/pages/pagina1_page.dart
+++ transicion_app/lib/pages/pagina1_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:transicion_app/pages/pagina2_page.dart';
+
 class Pagina1Page extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -13,8 +15,27 @@ class Pagina1Page extends StatelessWidget {
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon( Icons.access_time ),
-        onPressed: () {},
+        onPressed: () {
+          Navigator.push(context, _crearRuta());
+        },
       ),
+    );
+  }
+
+  Route _crearRuta() {
+    return PageRouteBuilder(
+      pageBuilder: (BuildContext context, Animation<double> animation, Animation<double> secondaryAnimation) => Pagina2Page(),
+      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+        final curvedAnimation = CurvedAnimation(parent: animation, curve: Curves.easeInOut);
+
+        return SlideTransition(
+          child: child,
+          position: Tween<Offset>(
+            begin: Offset(0, 1.0),
+            end: Offset.zero
+          ).animate(curvedAnimation)
+        );
+      },
     );
   }
 }
