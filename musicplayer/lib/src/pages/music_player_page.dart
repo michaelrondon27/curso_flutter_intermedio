e9085d28159315480939cd25407d4bb08ab0919c
michael rondon <mrondon72@gmail.com>
2021-08-05 20:26:12 +0200

    178. Imagen del disco

--- musicplayer/lib/src/pages/music_player_page.dart
+++ musicplayer/lib/src/pages/music_player_page.dart
@@ -8,9 +8,63 @@ class MusicPlayerPage extends StatelessWidget {
     return Scaffold(
       body: Column(
         children: [
-          CustomAppBar()
+          CustomAppBar(),
+
+          _ImagenDiscoDuration()
         ]
       )
+    );
+  }
+}
+
+class _ImagenDiscoDuration extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        children: [
+          _ImagenDisco()
+        ]
+      )
+    );
+  }
+}
+
+class _ImagenDisco extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular( 200 ),
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Image(image: AssetImage('assets/no-guidance.jpg')),
+
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular( 100 ),
+                color: Color(0xff997FED)
+              ),
+              height: 25,
+              width: 25
+            )
+          ]
+        )
+      ),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular( 200 ),
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          colors: [
+            Color(0xff997FED),
+            Color(0xff3621BA)
+          ]
+        )
+      ),
+      height: 250,
+      padding: EdgeInsets.all( 20 ),
+      width: 250
     );
   }
 }
